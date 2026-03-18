692ab058197635b5ac2ee64bd7bdd5369a4314ce
Prathikson <prathik@vincellstudios.com>
2026-03-17 21:09:37 -0600

    new3

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  optimizeFonts: true,
+  experimental: {
+    optimizePackageImports: ['gsap', 'framer-motion'],
+  },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+};
+
+module.exports = nextConfig;
